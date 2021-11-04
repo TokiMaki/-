6ac270fdc54d596fbc1b83aa80435e0ecf42d766
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2021-11-04 22:08:04 +0900

    Scene 클래스들 추가
    Scene 클래스들을 추가

--- tetris/tetris/Scene.cpp
+++ tetris/tetris/Scene.cpp
@@ -1,1 +1,13 @@
 #include "Scene.h"
+
+Scene::Scene() {}
+Scene::~Scene() {}
+
+TitleScene::TitleScene() {}
+TitleScene::~TitleScene() {}
+
+WaitScene::WaitScene() {}
+WaitScene::~WaitScene() {}
+
+PlayScene::PlayScene() {}
+PlayScene::~PlayScene() {}
--- tetris/tetris/Scene.h
+++ tetris/tetris/Scene.h
@@ -1,6 +1,36 @@
 #pragma once
+
 class Scene
 {
-	
+public:
+
+    Scene();
+    ~Scene();
+
+    void update();
 };
 
+class TitleScene : public Scene {
+
+    TitleScene();
+    ~TitleScene();
+
+    void update();
+};
+
+
+class WaitScene : public Scene {
+
+    WaitScene();
+    ~WaitScene();
+
+    void update();
+};
+
+class PlayScene : public Scene {
+
+    PlayScene();
+    ~PlayScene();
+
+    void update();
+};
