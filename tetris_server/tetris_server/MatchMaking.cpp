a3ba9818e3b189bb70049f795ca63ca270dac5ec
Hojin Kim <hojiny9923@gmail.com>
2021-11-06 23:10:26 +0900

    코드 정리
    Test code delete

--- tetris_server/tetris_server/MatchMaking.cpp
+++ tetris_server/tetris_server/MatchMaking.cpp
@@ -4,15 +4,7 @@
 
 DWORD WINAPI MatchMakingThread(LPVOID arg)
 {
-	//std::queue<SOCKADDR>* MatchMakingQ = (std::queue<SOCKADDR>*)arg;
-	std::queue<int>* test = (std::queue<int>*)arg;
-	std::cout << "Now runnning Match Making Thread :)\n";
-
-	while (!test->empty()) {
-		std::cout << test->front();
-		test->pop();
-	}
-	std::cout << "\nMatch Making Thread END :(\n";
+	std::queue<SOCKADDR>* MatchMakingQ = (std::queue<SOCKADDR>*)arg;
 	
 	return 0;
 }
