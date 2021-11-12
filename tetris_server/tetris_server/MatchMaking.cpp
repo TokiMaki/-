6c774a9dc76e771cff62de3b61a2434ce0ebc139
Hojin Kim <hojiny9923@gmail.com>
2021-11-13 03:49:04 +0900

    Send Wait Message to Client
    Wait 메세지를 클라이언트에 전송합니다.
    전송에 오류가 일어날 경우 에러 메세지를 화면에 출력하고 해당 클라이언트와 연결을 종료합니다.

--- tetris_server/tetris_server/MatchMaking.cpp
+++ tetris_server/tetris_server/MatchMaking.cpp
@@ -1,5 +1,6 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS // 최신 VC++ 컴파일 시 경고 방지
 #include "stdafx.h"
+#include "socket_function.h"
 #include "MatchMaking.h"
 
 
@@ -7,13 +8,14 @@ DWORD WINAPI MatchMakingThread(LPVOID arg)
 {
 	SOCKADDR_IN clientaddr;
 	int addrlen;
+	int retval;
 	std::vector<SOCKET>*MatchMakingQ = (std::vector<SOCKET>*)arg;
 
 	while (1) {
 		// 매칭 성립 알려줌
 		if (isMatchMakingQFull(MatchMakingQ)) {
-			CreateGameServerThread();
-			MatchMakingQ_DeQ();
+			CreateGameServerThread(MatchMakingQ);
+			MatchMakingQ_DeQ(MatchMakingQ);
 		}
 		else if(!MatchMakingQ->empty()) {
 			for (auto client : *MatchMakingQ) {
@@ -21,6 +23,26 @@ DWORD WINAPI MatchMakingThread(LPVOID arg)
 				getpeername(client, (SOCKADDR*)&clientaddr, &addrlen);
 				printf("[TCP 서버] 클라이언트 대기중: IP 주소=%s, 포트 번호=%d\n",
 					inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+
+				int MSG_len = htonl(sizeof(int));
+				int MSG = MSG_MatchMaking::Msg_WaitGame;
+				retval = send(client, (char*)&MSG_len, sizeof(int), 0);
+				if (retval == SOCKET_ERROR) {
+					err_display("send()");
+					MatchMakingQ->erase(std::remove_if(MatchMakingQ->begin(), MatchMakingQ->end(), 
+						[client](SOCKET target) {return target == client; }));
+					closesocket(client);
+					continue;
+				}
+
+				retval = send(client, (char*)&MSG, sizeof(int), 0);
+				if (retval == SOCKET_ERROR) {
+					err_display("send()");
+					MatchMakingQ->erase(std::remove_if(MatchMakingQ->begin(), MatchMakingQ->end(),
+						[client](SOCKET target) {return target == client; }));
+					closesocket(client);
+					continue;
+				}
 			}
 			printf("\n");
 		}
@@ -38,12 +60,12 @@ bool isMatchMakingQFull(std::vector<SOCKET>*MatchMakingQ)
 	else return false;
 }
 
-void CreateGameServerThread()
+void CreateGameServerThread(std::vector<SOCKET>* MatchMakingQ)
 {
 	std::cout << "Called CreateGameServerThread().\n";
 }
 
-void MatchMakingQ_DeQ()
+void MatchMakingQ_DeQ(std::vector<SOCKET>* MatchMakingQ)
 {
 	std::cout << "Called MatchMakingQ_DeQ().\n";
 }
--- tetris_server/tetris_server/MatchMaking.h
+++ tetris_server/tetris_server/MatchMaking.h
@@ -5,10 +5,10 @@ bool isMatchMakingQFull(std::vector<SOCKET>*MatchMakingQ);
 //현재 MatchMakingQ에서 대기하는 클라이언트의 개수를 파악해 MAX_PLAYER 미만인 경우 MatchMakingQ에 추가되어 있는 클라이언트들에게 Msg_wait을 보냄 
 //MAX_PLAYER 이상인 경우 MatchMakingQ에 추가되어 있는 순서대로 MAX_PLAYER개의 클라이언트에게 Msg_PlayInGame을 보내주고 CreateGameServerThread와 MatchMakingQ_DeQ를 실행
 
-void CreateGameServerThread();
+void CreateGameServerThread(std::vector<SOCKET>* MatchMakingQ);
 //GameServerThread를 생성하며, MatchMakingQ의 소켓들을 전달
 
-void MatchMakingQ_DeQ();
+void MatchMakingQ_DeQ(std::vector<SOCKET>* MatchMakingQ);
 //GameServerThread로 넘어간 MatchMakingQ에 클라이언트 소켓을 제거
 
 enum MSG_MatchMaking //매치매이킹 시스템에서 사용할 메시지
