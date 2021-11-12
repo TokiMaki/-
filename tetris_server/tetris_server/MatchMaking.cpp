0f900858c782ed5843cefb74efec3d98258bd6c4
Hojin Kim <hojiny9923@gmail.com>
2021-11-12 20:18:00 +0900

    MatchMakingThread
    일단 기본 틀만 작성

--- tetris_server/tetris_server/MatchMaking.cpp
+++ tetris_server/tetris_server/MatchMaking.cpp
@@ -5,7 +5,16 @@
 DWORD WINAPI MatchMakingThread(LPVOID arg)
 {
 	std::vector<SOCKET>* MatchMakingQ = (std::vector<SOCKET>*)arg;
-	
+	while (1) {
+		if (isMatchMakingQFull()) {
+			// 매칭 성립 알려줌
+			CreateGameServerThread();
+			MatchMakingQ_DeQ();
+		}
+		else {
+			// 대기하라 알려줌
+		}
+	}
 	return 0;
 }
 
