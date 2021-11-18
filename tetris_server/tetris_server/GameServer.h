0059f76d1a5a23533ffa92aa6ced4f5a101059fc
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2021-11-18 20:32:34 +0900

    GameServer.h수정
     다시 Struct로 변경

--- tetris_server/tetris_server/GameServer.h
+++ tetris_server/tetris_server/GameServer.h
@@ -8,9 +8,8 @@ struct Player
 	KeyInput m_key[MAX_PLAYER];//클라이언트의 키입력 저장
 	Gamestatus m_gamestatus[MAX_PLAYER];//게임의 상태 저장
 };
-class GameServerThreadData
+struct GameServerThreadData
 {
-public:
 	std::vector<SOCKET*> pClients; //MatchingThread에서 넘겨 받은 클라이언트 소켓들
 	std::vector<Player> pPlayers; //플레이어 개개인의 상태
 	//CGameTimer m_timer; //업데이트에서 프레임 시간 연산 시 사용
