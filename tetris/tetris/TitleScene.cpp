9d566ff47225b8058ac955b1743284cac667252d
Hojin Kim <hojiny9923@gmail.com>
2021-12-02 17:59:20 +0900

    Title Wait 배경색 변경

--- tetris/tetris/TitleScene.cpp
+++ tetris/tetris/TitleScene.cpp
@@ -19,22 +19,31 @@ void TitleScene::Paint(HDC hDC)
     int x = WINDOW_WIDTH / 4; //타이틀화면이 표시되는 x좌표
     int y = WINDOW_HEIGHT / 4; //타이틀화면이 표시되는 y좌표
 
-    HBRUSH myBrush = (HBRUSH)CreateSolidBrush(RGB(rand() % 255, rand() % 255, rand() % 255));
+    HBRUSH myBrush = (HBRUSH)CreateSolidBrush(RGB(2, 29, 106));
     HBRUSH oldBrush = (HBRUSH)SelectObject(hDC, myBrush);
+
+    Rectangle(hDC, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    SelectObject(hDC, oldBrush);
+    DeleteObject(myBrush);
+
+    myBrush = (HBRUSH)CreateSolidBrush(RGB(rand() % 255, rand() % 255, rand() % 255));
+    oldBrush = (HBRUSH)SelectObject(hDC, myBrush);
 
     Rectangle(hDC, x, y, x+250, y+100);
 
     SetBkMode(hDC, TRANSPARENT);
     SetTextColor(hDC, RGB(rand() % 255, rand() % 255, rand() % 255));
     TextOut(hDC, x + 20 * 5, y + 20 * 2, "T E T R I S", 11);
-    SetTextColor(hDC, RGB(0, 0, 0));
+    SetTextColor(hDC, RGB(255, 255, 255));
     TextOut(hDC, x, y + 20 * 7, "Enter Key to Start..", 20);
-    TextOut(hDC, x, y + 20 * 9, "  △   : Shift", 14);
+    TextOut(hDC, x, y + 20 * 9, "  △   : Rotate", 15);
     TextOut(hDC, x, y + 20 * 10, "◁  ▷ : Left / Right", 21);
     TextOut(hDC, x, y + 20 * 11, "  ▽   : Soft Drop", 18);
     TextOut(hDC, x, y + 20 * 12, " SPACE : Hard Drop", 18);
-    TextOut(hDC, x, y + 20 * 13, "  F4  : Quit", 12);
-    TextOut(hDC, x, y + 20 * 15, "BONUS FOR HARD DROPS / COMBOS", 29);
+    TextOut(hDC, x, y + 20 * 13, " Shift : Change Target", 22);
+    TextOut(hDC, x, y + 20 * 14, " Ctrl : Use Item", 16);
+    TextOut(hDC, x, y + 20 * 15, "  F4  : Quit", 12);
 
     SelectObject(hDC, oldBrush);
     DeleteObject(myBrush);
